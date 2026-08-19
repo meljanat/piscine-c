0abc182888d4fc5a7661543acf1084b2321bb87a
Mouad El-Janati <mel-jana@c2r2p12.1337.ma>
2026-08-19 18:11:46 +0100

    sixth ex

---
+++ c06/ft_sort_params.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-jana <mel-jana@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/14 23:55:59 by mouad             #+#    #+#             */
+/*   Updated: 2026/08/19 17:50:46 by mel-jana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	display(char **args, int n)
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j < n)
+	{
+		i = 0;
+		while (args[j][i] != '\0')
+		{
+			write(1, &args[j][i], 1);
+			i++;
+		}
+		write(1, "\n", 1);
+		j++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+	display(argv, argc);
+	return (0);
+}
